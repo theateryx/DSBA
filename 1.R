97a575e9b2435efe359f6f2961412ad0df779e4b
Ping <rockerfella@gmail.com>
2020-03-09 21:21:58 +0800

    9.21

--- 1.R
+++ 1.R
@@ -1,15 +1,15 @@
-setwd("C:/x")
+setwd("C:/Users/Ping/Documents/GitHub/dsba")
 library(lubridate)
 library(tidyverse)
 library(readxl)
-library(arulez)
 library(dplyr)
-library(xlsx)
-library(arulezviz)
+library(arulesViz)
 library(arules)
 library(lattice)
+library(DataExplorer)
 
-data <- read_xlsx("sundata.xlsx")
+
+data <- read_xlsx("Nov.xlsx")
 View(data)
 str(data)
 summary(data)
@@ -27,14 +27,45 @@ cols <- c("Date","Transaction","CategoryID","Category", "ClassID","Class", "ID",
 sun <-data1 %>% mutate_at(cols, factor)
 head(sun)
 sun$Day <- as.factor(sun$Day)
+
+# check NA
+anyNA(sun)
+colSums(is.na(sun))
+
 summary(sun)
+
+# Subset out free items
+
+
+
+# EDA ---------------------------------------------------------------------
+
+#univariate
+attach(sun)
+plot(Date)
+plot(Day)
+plot(Transaction)
+plot(CategoryID)
+plot(Category)
+plot(ClassID)
+plot(ID)
+plot(Menu)
+plot(Qty)
+boxplot(Price)
+qplot(CostAmt)
+plot(CostPC)
+plot_histogram(sun, title ="Univariate Analysis for Continuous Data")
+plot_bar(sun, title ="Univariate Analysis for Factored Data")
+
+
+
+which.max(Price)
+
+# datamining --------------------------------------------------------------
 
 # split txn
 sun.agg <- split(sun$Menu, sun$Transaction)
 head(sun.agg)
-
-class(sun.agg)
-sun.agg
 
 # remove dupes in each txn
 sun.agg1 <- list()
@@ -42,6 +73,5 @@ sun.agg1 <- list()
    sun.agg1[[i]] <- as.character(sun.agg[[i]][!duplicated(sun.agg[[i]])])
  }
 
-Txns <- as(sun.agg1, "Menu1")
 
-summary(Txns)
+??arules
